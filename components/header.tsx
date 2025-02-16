0fb4ca7a618c5b241d356a53858a2c23a0d01268
Jason Fu <jasonfu6699@hotmail.com>
2025-02-15 18:47:31 -0800

    fix some css

--- components/header.tsx
+++ components/header.tsx
@@ -2,9 +2,13 @@ import { ThemeToggle } from "./theme-toggle";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full">
-      <h1>Cooking Companion</h1>
-      <ThemeToggle />
+    <header className="grid grid-cols-12 justify-between items-center w-full sticky mt-12 px-4 bg-background">
+      <div className="col-span-4 lg:col-span-4"></div>
+      <div className="col-span-8 lg:col-span-4 flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Cooking Companion</h1>
+        <ThemeToggle />
+      </div>
+      <div className="hidden lg:block lg:col-span-4"></div>
     </header>
   );
 }
