66edcd48c4c6a5b1874bb1e1314b0c86886c5559
Vinicio Sanchez-Mason <vinicio.sanchez@tempo.io>
2025-03-27 19:20:18 -0700

    multiplier buttons

--- components/conversion-form.tsx
+++ components/conversion-form.tsx
@@ -1,15 +1,10 @@
-import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
-import { formatNumber } from "@/lib/utils";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { ConversionType, Unit } from "@/lib/constants";
+import {ChangeEvent, Dispatch, SetStateAction, useState} from "react";
+import {applyMultiplier, formatNumber} from "@/lib/utils";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue,} from "@/components/ui/select";
+import {ConversionType, Unit} from "@/lib/constants";
+import {Button} from "@/components/ui/button";
 
 const UnitDropdownOptions = ({ units }: { units: ReadonlyArray<Unit> }) => {
   return (
@@ -31,6 +26,9 @@ interface ConversionFormProps {
   convert: (input: number, from: Unit, to: Unit) => number;
   conversionTable: React.ReactNode;
 }
+
+const MULTIPLIERS = [2, 3, 4, 5];
+const FRACTIONAL_MULTIPLIERS = [5, 4, 3, 2];
 
 export const ConversionForm = ({
   type,
@@ -56,18 +54,33 @@ export const ConversionForm = ({
   const formattedResult =
     typeof result === "number" ? formatNumber(result) : result;
 
+  const getHandleMultiplier = (multiplier: number) => () =>  {
+    if(input != '') {
+      const multipliedInput = applyMultiplier(input, multiplier);
+      setInput(multipliedInput.toString());
+    }
+  }
+
   return (
     <>
       <div className="space-y-4">
         <div className="space-y-2">
           <Label htmlFor={`${type}-input`}>Value</Label>
-          <Input
-            autoFocus
-            id={`${type}-input`}
-            type="number"
-            value={input}
-            onChange={handleInputChange}
-          />
+          <div className="flex items-center space-x-1">
+            {FRACTIONAL_MULTIPLIERS.map((multiplier) => (
+                <Button variant="secondary" key={`multiplier-1/${multiplier}`} size="sm" onClick={getHandleMultiplier(1/multiplier)}>1/{multiplier}</Button>))
+            }
+            <Input
+                autoFocus
+                id={`${type}-input`}
+                type="number"
+                value={input}
+                onChange={handleInputChange}
+            />
+            {MULTIPLIERS.map((multiplier) => (
+                <Button variant="secondary" key={`multiplier-${multiplier}`} onClick={getHandleMultiplier(multiplier)} size="sm">x{multiplier}</Button>))
+            }
+          </div>
         </div>
         <div className="grid grid-cols-2 gap-4">
           <div className="space-y-2">
