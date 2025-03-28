3026900e8230718335f748cde6f552f3647851ab
Jason Fu <jasonfu6699@hotmail.com>
2025-03-28 12:38:44 -0700

    put gtm in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,10 +35,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-PT5Z96NM" />
+      <head>
+        <GoogleTagManager gtmId="GTM-PT5Z96NM" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-PT5Z96NM`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
